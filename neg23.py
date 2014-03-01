f986ba280b8a0a4ecce33ce2233193d91458c44b
Kyle Swanson <kylophone@gmail.com>
2014-03-01 09:40:19 -0600

    Update neg23.py

--- neg23.py
+++ neg23.py
@@ -2,6 +2,7 @@
 import os, sys, subprocess, re
 if len(sys.argv) == 2:
 	
+	print "Processing..."
 	#Passes the audiofile to FFmpeg, parses the output and saves the loudness as a string called loudness.
 	AudioFileIn = re.escape(sys.argv[1])
 	proc = subprocess.Popen("ffmpeg -nostats -i " + AudioFileIn + " -filter_complex ebur128 -f null - 2>&1 | tail | grep I: | tr -d I: | tr -d LUFS | tr -d \ ", shell=True, stdout=subprocess.PIPE)
